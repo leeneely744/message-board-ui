cfddb4abe399c4cae80097b5d1269045559e2aef
kohei sano <nlee9337@gmail.com>
2025-06-14 20:40:51 +0900

    Add contract integration and message fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import { ethers } from "ethers";
+
+import { abi, CONTRACT_ADDRESS, CONTRACT_ABI } from './contract';
 
 function App() {
   const [account, setAccount] = useState(null);
+  const [messages, setMessages] = useState([]);
 
   const connectWallet = async () => {
     if (!window.ethereum) {
@@ -19,6 +22,19 @@ function App() {
       console.error("接続エラー:", err);
     }
   };
+  
+  const fetchMessages = async () => {
+    // ethers.js を使ってコントラクトを初期化
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const messageBoardContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      provider
+    );
+
+    const newMessages = await messageBoardContract.getLatestMessages(5);
+    setMessages(newMessages);
+  }
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
