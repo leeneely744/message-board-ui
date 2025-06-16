133f11ebfb1e829d9c4c23094117e731043dbadf
kohei sano <nlee9337@gmail.com>
2025-06-16 22:26:45 +0900

    Implement message posting functionality and adjust button placement

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,26 @@ function App() {
     setMessages(newMessages);
   }
 
+  const postMessage = async () => {
+    if (!newMessage) {
+      alert("メッセージが空です。");
+      return;
+    }
+    
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+
+      const tx = await contract.postMessage(newMessage);
+      await tx.wait();
+    } catch (error) {
+      console.error("投稿失敗: ", err);
+      alert("投稿に失敗しました。");
+    }
+  };
+
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>MessageBoard</h1>
@@ -46,7 +66,6 @@ function App() {
       {account ? (
         <>
           <p>接続済みアカウント: {account}</p>
-          <button onClick={fetchMessages}>最新メッセージを取得</button>
           <input
             type="text"
             value={newMessage}
@@ -54,6 +73,8 @@ function App() {
             placeholder="input message"
           />
           <button onClick={postMessage}>投稿する</button>
+          <br/>
+          <button onClick={fetchMessages}>最新メッセージを取得</button>
           <ul>
             {messages.map((msg, index) => (
               <li key={index}>{msg.text} at {new Date(Number(msg.timestamp) * 1000).toLocaleString()}</li>
