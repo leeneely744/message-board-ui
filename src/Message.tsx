3a6393d4479cf019624a6bd2bb25736726bf726a
kohei sano <nlee9337@gmail.com>
2025-06-27 23:58:41 +0900

    Fix rendering of author controls in Message component

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,12 +2,12 @@ export function Message({ index, message, timestamp, isAuthor }) {
     var tsForLi = "at " + new Date(Number(timestamp) * 1000).toLocaleString();
     return (
         <li key={index}>
-            {message} {tsForLi} (isAuthor && {
+            {message} {tsForLi} {isAuthor && 
                 <>
                     <button>Edit</button>
                     <button>Delete</button>
                 </>
-            })
+            }
         </li>
     )
 }
